ccf0ae98b7d0d48b91d373aa617a4b5fbe0cdf97
jeffliulab <pangliu@brandeis.edu>
2024-09-12 02:02:33 +0000

    update

--- src/ros1/src/basic_mover.py
+++ src/ros1/src/basic_mover.py
@@ -4,45 +4,69 @@ import rospy
 from geometry_msgs.msg import Point, Pose, Twist
 from tf.transformations import euler_from_quaternion
 
-MAX_TIME_SECONDS = 60
-SPEED = 0.3
-RUN_LOOP_HZ = 10
-
-############ LAB SEP 6
-
 # BasicMover
 class BasicMover:
     def __init__(self):
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-        self.twist = Twist()
+        self.my_odom_sub = rospy.Subscriber('my_odom', Point, self.my_odom_cb)
+        # Current heading of the robot.
+        self.cur_yaw = None
+        # Current distance moved of the robot.
+        self.start_distance = None
+        self.moved_distance = 0.0
 
     def my_odom_cb(self, msg):
         """Callback function for `self.my_odom_sub`."""
-        raise NotImplementedError
+        # my_odom Node计算完成后，会发送 Point message回来（回到basic_mover这儿来）
+        # 复习：Point message（geometry_msgs/Point）包括x, y, z三个浮点数，可以用来表示机器人的位置
+        self.moved_distance = msg.x # moved distance, received from my_odom topic
+        self.cur_yaw = msg.y # yaw, received from my_odom topic
+
 
     def turn_to_heading(self, target_yaw):
         """
         Turns the robot to heading `target_yaw`.
         """
-        raise NotImplementedError
-        
-    def move_forward(self, target_dist):
-        """Moves the robot forward by `target_dist`."""
-        ### Sep.6 #################################
+        # 这个函数的目的是让机器人从现在的yaw旋转到目标yaw
         twist = Twist()
         rate = rospy.Rate(10)
-        twist.linear.x = 0.1
-        move_time = target_dist / twist.linear.x
 
-        start_time = rospy.Time.now()
-        while (rospy.Time.now() - start_time).to_sec() < move_time:
+        #如果误差角度很小，停止旋转
+        while not rospy.is_shutdown():
+            delta_yaw = target_yaw - self.cur_yaw
+            if abs(delta_yaw) < 0.05:
+                twist.angular.z = 0.0
+                self.cmd_vel_pub.publish(twist)
+                break 
+            
+            # 根据偏差方向旋转
+            twist.angular.z = 0.1 if delta_yaw >0 else -0.1
+            self.cmd_vel_pub.publish(twist)
+            rate.sleep() 
+
+    def move_forward(self, target_dist):
+        """Moves the robot forward by `target_dist`."""
+        twist = Twist()
+        rate = rospy.Rate(10)
+
+        self.start_distance = self.moved_distance
+
+        twist.linear.x = 0.1
+        while not rospy.is_shutdown():
+            # 计算当前距离相对于起点的变化
+            delta_distance = self.moved_distance - self.start_distance 
+            # 如果移动的距离达到目标，则停止移动
+            if delta_distance >= target_dist:
+                break 
+            # 否则继续移动
             self.cmd_vel_pub.publish(twist)
             rate.sleep()
+        
+        # 停止移动
         twist.linear.x = 0.0
         self.cmd_vel_pub.publish(twist)
+            
 
-        ###
-        #raise NotImplementedError
 
     def out_and_back(self, target_dist):
         """
@@ -51,20 +75,29 @@ class BasicMover:
         2. turns the robot by 180 degrees; and
         3. moves the robot forward by `target_dist`.
         """
-        ### Sep.6 #################################
+        # 这个函数实现作业中要求的前进1m，调转车头，然后后退1m
         self.move_forward(target_dist)
-        ###
 
-        #raise NotImplementedError
+        target_yaw = self.cur_yaw + math.pi # math.pi means 180 degrees，在ROS中，角度使用的是弧度制
+        if target_yaw > math.pi:
+            target_yaw -= 2 * math.pi #......
+        
+        self.turn_to_heading(target_yaw)
+        self.move_forward(target_dist)
+        
+
+
 
     def draw_square(self, side_length):
         """
         This function moves the robot in a square with `side_length` meter sides.
         """ 
+        # 这个函数实现作业中要求的画方块
         raise NotImplementedError
 
     def move_in_a_circle(self, r):
         """Moves the robot in a circle with radius `r`"""
+        # 这个函数实现作业中的画圈圈
         raise NotImplementedError
         
     def rotate_in_place(self):
@@ -77,26 +110,10 @@ class BasicMover:
             twist.angular.z = 0.1
             self.cmd_vel_pub.publish(twist)
             rate.sleep()
-    
-    def run(self):
-        rate = rospy.Rate(RUN_LOOP_HZ)
-
-        while not rospy.is_shutdown():
-            
-            if True:
-                self.twits.linear.x = SPEED/2.0 # accelerating
-            elif False:
-                pass
-                # decelarating
-            else:
-                return
-            self.cmd_vel_pub.publish(self.twist)
-            rate.sleep()
-
 
 if __name__ == '__main__':
     rospy.init_node('basic_mover')
-    BasicMover().out_and_back(500)
+    BasicMover().out_and_back(1)
     # BasicMover().draw_square(1)
     # BasicMover().move_in_a_circle(1)
     # BasicMover().rotate_in_place()
--- src/ros1/src/my_odom.py
+++ src/ros1/src/my_odom.py
@@ -16,6 +16,8 @@ class MyOdom:
                 
     def odom_cb(self, msg):
         """Callback function for `odom_sub`."""
+        # 复习：Odometry message (nav_msgs/Odometry)包含机器人的pose位置、orientation方向、以及twist（包含线速度和角速度）
+        # my_odom node通过odom topic接收TurtleBot3发布的Odometry message
         cur_pose = msg.pose.pose
         self.update_dist(cur_pose) 
         self.update_yaw(cur_pose.orientation)
@@ -27,14 +29,25 @@ class MyOdom:
         Updates `self.dist` to the distance between `self.old_pose` and
         `cur_pose`.
         """
-        raise NotImplementedError
+        #######################################################
+        # 这个函数接收距离，然后根据距离计算累计的移动距离，这里的单位是米
+        if self.old_pose is not None:
+            x_move = cur_pose.position.x - self.old_pose.x 
+            y_move = cur_pose.position.y - self.old_pose.y 
+            self.dist += math.sqrt(x_move**2 + y_move**2)
+
+        self.old_pose = cur_pose.position # update old position
 
     def update_yaw(self, cur_orientation):
         """
         Helper to `odom_cb`.
         Updates `self.yaw` to current heading of robot.
         """
-        raise NotImplementedError
+        #########################################################
+        # 四元数quaternion = w + xi + yj + zk， 用ROS自带的转换函数可以将四元数转换为欧拉角
+        cur_quaternion = [cur_orientation.x, cur_orientation.y, cur_orientation.z, cur_orientation.w]
+        (roll, pitch, yaw) = euler_from_quaternion(cur_quaternion)
+        self.yaw = yaw # 更新当前的偏航角
 
     def publish_data(self):
         """
@@ -42,16 +55,12 @@ class MyOdom:
         """
         # The `Point` object should be used simply as a data container for
         # `self.dist` and `self.yaw` so we can publish it on `my_odom`.
+        p = Point() # p is a Point message
+        p.x = self.dist 
+        p.y = self.yaw
+        p.z = 0.0 # Point message has x,y,z three float64 variables
+        self.my_odom_pub.publish(p)
         
-        #########################
-        data = Point()
-        data.x = self.dist
-        data.y = self.turn
-        
-
-
-
-
         
 if __name__ == '__main__':
     rospy.init_node('my_odom')
